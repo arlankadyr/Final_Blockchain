2e0c429b2728bc12ff1f238c2e63ed1766c95f52
arlankadyr <241912@astanait.edu.kz>
2026-02-08 18:02:45 +0500

    Refactor ERC20 implementation and add RewardToken

--- ERC20.sol
+++ ERC20.sol
@@ -1,77 +1,16 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-interface IERC20 {
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint256);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-}
+contract RewardToken is ERC20, Ownable {
+    constructor(string memory name_, string memory symbol_)
+        ERC20(name_, symbol_)
+        Ownable(msg.sender)
+    {}
 
-contract ERC20 is IERC20 {
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-
-    uint256 public override totalSupply;
-    mapping(address => uint256) public override balanceOf;
-    mapping(address => mapping(address => uint256)) public override allowance;
-
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-
-    constructor(
-        string memory _name,
-        string memory _symbol
-    ) {
-        name = _name;
-        symbol = _symbol;
-        decimals = 18;
-    }
-
-    function transfer(address recipient, uint256 amount) public override returns (bool) {
-        require(balanceOf[msg.sender] >= amount, "Not enough balance");
-
-        balanceOf[msg.sender] -= amount;
-        balanceOf[recipient] += amount;
-        
-        emit Transfer(msg.sender, recipient, amount);
-        return true;
-    }
-
-
-    function approve(address spender, uint256 amount) public override returns (bool) {
-        allowance[msg.sender][spender] = amount;
-
-        emit Approval(msg.sender, spender, amount);
-        return true;
-    }
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) public override returns (bool) {
-        require(balanceOf[sender] >= amount, "Not enough balance");
-        require(allowance[sender][msg.sender] >= amount, "Allowance exceeded");
-
-        allowance[sender][msg.sender] -= amount;
-        balanceOf[sender] -= amount;
-        balanceOf[recipient] += amount;
-
-        emit Transfer(sender, recipient, amount);
-        return true;
-    }
-
-    function _mint(address to, uint256 amount) internal {
-        totalSupply += amount;
-        balanceOf[to] += amount;
-
-        emit Transfer(address(0), to, amount);
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
     }
 }
-
